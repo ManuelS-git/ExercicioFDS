51f564d6a65f3b79fa989c684472d71bec46bb85
Manuel <manuel.soares@edu.pucrs.br>
2025-08-11 21:26:08 +0000

    primeiraAplicacao

---
+++ AtividadeCod/src/main/java/fds/manuel/App.java
@@ -0,0 +1,7 @@
+package fds.manuel;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto AtividadeCod criado com sucesso.");
+    }
+}
