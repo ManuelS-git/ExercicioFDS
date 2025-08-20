37aa3a17ec1015fc7188215adcb7abb2b143910c
Manuel <manuel.soares@edu.pucrs.br>
2025-08-20 20:14:12 +0000

    Alteração no main para aparecer codificações

--- AtividadeCod/src/main/java/fds/manuel/App.java
+++ AtividadeCod/src/main/java/fds/manuel/App.java
@@ -3,22 +3,33 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Codificador cod = new CodificadorSimples();
+        Codificador morse = new CodeMorse();
+        Codificador vigen = new CodificadorVigenere();
         Factory fab = new Factory();
         Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Codificador: "+cod.getNome());
         //System.out.println("Versao: "+cod.getDataCriacao());
         System.out.println("Digite o nivel de segurança desejado)");
         int nivel = teclado.nextInt();
         System.out.println("Melhor Nivel de segurança: "+ fab.getCodificadores(nivel));
         
-        String texto = "Este e o string a ser codificado";
-        String codificado = cod.codifica(texto);
-        String decodificado = cod.decodifica(codificado);
+        System.out.println("////////////////////////////////////");
 
+        System.out.println("Exemplo com o código morse");
+        String texto = "Este e o string a ser codificado";
+        String codificado = morse.codifica(texto);
+        String decodificado = morse.decodifica(codificado);
+        System.out.println("Texto original: "+texto);
+        System.out.println("Texto codificado: "+ codificado);
+        System.out.println("Texto decodificado: "+decodificado);
+
+        System.out.println("////////////////////////////////////");
+
+        System.out.println("Exemplo com o código Vigenere");
+        codificado = vigen.codifica(texto);
+        decodificado = vigen.decodifica(codificado);
         System.out.println("Texto original: "+texto);
         System.out.println("Texto codificado: "+codificado);
         System.out.println("Texto decodificado: "+decodificado);
+        
     }
 }
